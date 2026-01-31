3d68ff16ba6a26c8b66dfac64609132b66285df8
Ivan Zhirnov <i.zhirnov@datasakura.com>
2026-01-31 18:35:47 +0700

    Added extentions with scales

--- Assets/Scripts/ECS/ActionChain/LocalTransformExtensions.cs
+++ Assets/Scripts/ECS/ActionChain/LocalTransformExtensions.cs
@@ -17,5 +17,27 @@ public static class LocalTransformExtensions
     {
         return transform.Position.IsDistanceGreaterThan(position, distance);
     }
+
+    public static bool IsTargetReached(this LocalTransform transform, float3 position, float scale, float distance = 0.01f)
+    {
+        float reachThreshold = (transform.Scale + scale) * 0.5f + distance;
+        return math.distancesq(transform.Position, position) <= reachThreshold * reachThreshold;
+    }
+
+    public static bool IsTargetReached(this LocalTransform transform, LocalTransform other, float distance = 0.01f)
+    {
+        return transform.IsTargetReached(other.Position, other.Scale, distance);
+    }
+
+    public static bool IsDistanceGreaterThan(this LocalTransform transform, float3 position, float scale, float distance)
+    {
+        float distanceThreshold = (transform.Scale + scale) * 0.5f + distance;
+        return math.distancesq(transform.Position, position) > distanceThreshold * distanceThreshold;
+    }
+
+    public static bool IsDistanceGreaterThan(this LocalTransform transform, LocalTransform other, float distance)
+    {
+        return transform.IsDistanceGreaterThan(other.Position, other.Scale, distance);
+    }
 }
 
